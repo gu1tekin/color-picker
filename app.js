4fd98fd5109daa3a20d9aec894a38183d4159776
gu1tekin <gultekincirik@gmail.com>
2021-03-22 16:17:37 +0100

    added new colors and reset button

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $("div").hover(function () {
+  $("div").click(function () {
     let bgColor = $(this).attr("color");
     $("body").css("background", bgColor);
   });

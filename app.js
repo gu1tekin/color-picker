61da1c70875978b93492d557910b2afa59ade37e
gu1tekin <gultekincirik@gmail.com>
2021-03-24 22:43:51 +0100

    bug fix for reset button

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
   $("div").click(function () {
     let bgColor = $(this).attr("color");
-    $("body").css("background", bgColor);
+    $("table").css("background", bgColor);
   });
 });
+
+// hide #color-accessibility && #reset-color
+// function hiddeColoAccessibility() {
+//   document.querySelector("#color-accessibility").style.visibility = "hidden";
+// }
+
+// show #color-accessibility && #reset-color
+// function showColoAccessibility() {
+//   document.querySelector("#color-accessibility").style.visibility = "visible";
+//   document.querySelector("#reset-color").style.visibility = "visible";
+// }

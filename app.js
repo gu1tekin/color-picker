a140acb19f32b8925b559d16b4ddf94dc8313485
gu1tekin <gultekincirik@gmail.com>
2021-03-25 02:59:36 +0100

    added a funchion to change color code and bg

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-  $("div").click(function () {
-    let bgColor = $(this).attr("color");
-    $("table").css("background", bgColor);
-  });
-});
+// $(document).ready(function () {
+//   $("div").click(function () {
+//     let bgColor = $(this).attr("color");
+//     $("table").css("background", bgColor);
+//   });
+// });
 
 // hide #color-accessibility && #reset-color
 // function hiddeColoAccessibility() {
@@ -15,3 +15,32 @@ $(document).ready(function () {
 //   document.querySelector("#color-accessibility").style.visibility = "visible";
 //   document.querySelector("#reset-color").style.visibility = "visible";
 // }
+
+// adding dynamic attributes like colorCode...
+function showColorCode(colorID, Gultkein) {
+  if (colorID === "colorIndigo") {
+    document.querySelector("#color-code").innerHTML =
+      "#49599b " + " <i class='far fa-copy'>";
+    document.querySelector("table").style.background = "#49599b";
+  } else if (colorID === "colorAmber") {
+    document.querySelector("#color-code").innerHTML =
+      "#ffa726 " + " <i class='far fa-copy'>";
+    document.querySelector("table").style.background = "#ffa726";
+  } else if (colorID === "colorOrange") {
+    document.querySelector("#color-code").innerHTML =
+      "#e65100 " + " <i class='far fa-copy'>";
+    document.querySelector("table").style.background = "#e65100";
+  } else if (colorID === "colorGreen") {
+    document.querySelector("#color-code").innerHTML =
+      "#2e7d32 " + " <i class='far fa-copy'>";
+    document.querySelector("table").style.background = "#2e7d32";
+  } else if (colorID === "colorBlueGrey") {
+    document.querySelector("#color-code").innerHTML =
+      "#263238 " + " <i class='far fa-copy'>";
+    document.querySelector("table").style.background = "#263238";
+  } else if (colorID === "colorReset") {
+    document.querySelector("#color-code").innerHTML =
+      "#FFFFFF " + " <i class='far fa-copy'>";
+    document.querySelector("table").style.background = "#FFFFFF";
+  }
+}
